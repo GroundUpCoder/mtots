975c8b55e5ee63718e25579976d3a6ac518f5768
Wombat Nomad <116476627+GroundUpCoder@users.noreply.github.com>
2022-11-04 13:44:06 +0700

    delete the old old mtots impl

--- misc/old/src/bin/mtots.c
+++
@@ -1,9 +0,0 @@
-#include "mtots.h"
-#include <stdio.h>
-
-int main() {
-  mtots_State *L = mtots_newstate();
-  printf("START\n");
-  mtots_panicf(L, "%% Hello %d ... (%s) %% %f", 10 + 3, "some string", 24.33);
-  return 0;
-}
--- misc/old/src/src/mtotsobject.c
+++
@@ -1,2 +0,0 @@
-#include "mtotsobject.h"
-
