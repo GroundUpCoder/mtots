4dea6eb72ccf0a328779125272916be52e1e20c4
Wombat Nomad <116476627+GroundUpCoder@users.noreply.github.com>
2022-11-18 14:21:04 +0700

    Update mtots_util.h

--- src/mtots_util.h
+++ src/mtots_util.h
@@ -4,6 +4,7 @@
 /* Collection of self contained utilities */
 /* TODO: make `panic` and `runtimeError` also self contained */
 
+#include "mtots_util_string.h"
 #include "mtots_util_error.h"
 #include "mtots_util_unicode.h"
 #include "mtots_util_strbuf.h"
