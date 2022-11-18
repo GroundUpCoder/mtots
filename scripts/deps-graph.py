d9cc40279fafdd52ddd781bf4b2db51edda9c627
Wombat Nomad <116476627+GroundUpCoder@users.noreply.github.com>
2022-11-18 17:41:47 +0700

    Update deps-graph.py

--- scripts/deps-graph.py
+++ scripts/deps-graph.py
@@ -46,7 +46,11 @@ def getDeps(fileName):
 def formatName(name):
   if name.endswith('.h') and name.startswith('mtots_'):
     return name[len('mtots_'):-len('.h')]
-  return name.replace('.', '_')
+  if name.endswith('.c') and name.startswith('mtots_'):
+    return name[len('mtots_'):]
+  if name == 'main.c':
+    return 'MAIN'
+  raise Exception(f'UNRECOGNIZED NAME PATTERN {name}')
 
 depsMap = {
   formatName(fileName): sorted(formatName(d) for d in getDeps(fileName)) for fileName in fileNames
@@ -54,21 +58,21 @@ depsMap = {
 
 # Remove the connection where source files depend on their corresponding header
 for target in depsMap:
-  if not (target.startswith('mtots_') and target.endswith('_c')):
+  if not target.endswith('.c'):
     continue
-  basename = target[len('mtots_'):-len('_c')]
+  basename = target[:-len('.c')]
   if basename in depsMap[target]:
     depsMap[target].remove(basename)
 
 emit("digraph headerDeps {")
 
 for target in depsMap:
-  if target.endswith('_c'):
-    emit(f'  {target} [shape=box]')
+  if target.endswith('.c'):
+    emit(f'  "{target}" [shape=box]')
 
 for target in depsMap:
   for dep in depsMap[target]:
-    emit(f'  {target} -> {dep}')
+    emit(f'  "{target}" -> "{dep}"')
 emit("}")
 
 if outfilePath:
