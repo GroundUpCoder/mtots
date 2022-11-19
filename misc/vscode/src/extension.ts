90a2c4eb7b166be363ba5247197f9c9ddd3255fa
Wombat Nomad <116476627+GroundUpCoder@users.noreply.github.com>
2022-11-19 11:54:40 +0700

    vscode commands.ts

--- misc/vscode/src/extension.ts
+++ misc/vscode/src/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+import * as commands from './commands';
 import { ExtensionContext, Location } from 'vscode';
 import { Parser } from './lang/parser';
 import { MScanner, MScannerError } from './lang/scanner';
@@ -8,56 +9,5 @@ export function activate(context: ExtensionContext) {
 
   context.subscriptions.push(vscode.commands.registerCommand(
     'mtots.tokenize',
-    async () => {
-      const editor = vscode.window.activeTextEditor;
-      if (!editor) {
-        return;
-      }
-      const selection =
-        editor.selection.isEmpty ?
-          new vscode.Range(
-            editor.document.lineAt(0).range.start,
-            editor.document.lineAt(editor.document.lineCount - 1).range.end) :
-          new vscode.Range(
-            editor.selection.start,
-            editor.selection.end);
-      const text = editor.document.getText(selection);
-      const scanner = new MScanner(text);
-
-      const document = await vscode.workspace.openTextDocument({
-        content: '',
-        language: 'plaintext',
-      });
-
-      let insertText = '';
-
-      try {
-        while (true) {
-          const token = scanner.scanToken();
-          insertText += (
-            `${token.range.start.line + 1}:` +
-            `${token.range.start.column + 1} - ` +
-            `${token.range.end.line + 1}:` +
-            `${token.range.end.column + 1} - ` +
-            `${token.type} ` +
-            `${token.value === null ? '' : JSON.stringify(token.value)}\n`);
-          if (token.type === 'EOF') {
-            break;
-          }
-        }
-      } catch (e) {
-        if (!(e instanceof MScannerError)) {
-          throw e;
-        }
-        insertText += `ERROR: ${e.message}`;
-        insertText += `  ${e.range.start.line + 1}:${e.range.start.column + 1}`;
-      }
-
-      const edit = new vscode.WorkspaceEdit();
-      edit.insert(document.uri, new vscode.Position(0, 0), insertText);
-      if (await vscode.workspace.applyEdit(edit)) {
-        vscode.window.showTextDocument(document);
-      }
-    }
-  ));
+    commands.tokenize));
 }
