4b5a8419d32b95dd551d0d88eb816e3665ec1e38
Wombat Nomad <116476627+GroundUpCoder@users.noreply.github.com>
2022-11-19 22:33:49 +0700

    vscode: more AST and parser WIP

--- misc/vscode/src/lang/ast.ts
+++ misc/vscode/src/lang/ast.ts
@@ -1,15 +1,286 @@
 import { MLocation } from "./location";
 
+type LogicalOperator = (
+  'not'     |
+  'and'     |
+  'or'
+)
 
-export abstract class MAst {
+export abstract class Ast {
   location: MLocation;
   constructor(location: MLocation) {
     this.location = location;
   }
 }
 
-export class Nop extends MAst {
+export abstract class Statement extends Ast {}
+
+export abstract class Expression extends Ast {}
+
+/**
+ * All type expressions boil down to operator[Arg1, Arg2, ...]
+ * in some form.
+ *
+ * Some syntactic sugar:
+ *   SomeType? => optional[SomeType]
+ *   A.B       => member[A, B]
+ *
+ */
+export class TypeExpression extends Ast {
+  name: string;
+  args: TypeExpression[];
+  constructor(location: MLocation, name: string, args: TypeExpression[]) {
+    super(location);
+    this.name = name;
+    this.args = args;
+  }
+}
+
+export class Nop extends Statement {
   constructor(location: MLocation) {
     super(location);
+  }
+}
+
+export class Parameter extends Ast {
+  name: string;
+  type: TypeExpression;
+  defaultValue: Expression | null;
+  constructor(
+      location: MLocation,
+      name: string,
+      type: TypeExpression,
+      defaultValue: Expression | null) {
+    super(location);
+    this.name = name;
+    this.type = type;
+    this.defaultValue = defaultValue;
+  }
+}
+
+export class Function extends Statement {
+  name: string;
+  parameters: Parameter[];
+  returnType: TypeExpression | null;
+  body: Block;
+  constructor(
+      location: MLocation,
+      name: string,
+      parameters: Parameter[],
+      returnType: TypeExpression | null,
+      body: Block) {
+    super(location);
+    this.name = name;
+    this.parameters = parameters;
+    this.returnType = returnType;
+    this.body = body;
+  }
+}
+
+export class Field extends Ast {
+  name: string;
+  type: TypeExpression;
+  constructor(location: MLocation, name: string, type: TypeExpression) {
+    super(location);
+    this.name = name;
+    this.type = type;
+  }
+}
+
+export class Class extends Statement {
+  name: string;
+  bases: Expression[];
+  fields: Field[];
+  methods: Function[];
+  constructor(
+      location: MLocation,
+      name: string,
+      bases: Expression[],
+      fields: Field[],
+      methods: Function[]) {
+    super(location);
+    this.name = name;
+    this.bases = bases;
+    this.fields = fields;
+    this.methods = methods;
+  }
+}
+
+export class Import extends Statement {
+  moduleName: string;
+  alias: string | null;
+  constructor(
+      location: MLocation,
+      moduleName: string,
+      alias: string | null) {
+    super(location);
+    this.moduleName = moduleName;
+    this.alias = alias;
+  }
+}
+
+export class VariableDeclaration extends Statement {
+  final: boolean;
+  name: string;
+  type: TypeExpression;
+  value: Expression;
+  constructor(
+      location: MLocation,
+      final: boolean,
+      name: string,
+      type: TypeExpression,
+      value: Expression) {
+    super(location);
+    this.final = final;
+    this.name = name;
+    this.type = type;
+    this.value = value;
+  }
+}
+
+export class While extends Statement {
+  condition: Expression;
+  body: Block;
+  constructor(location: MLocation, condition: Expression, body: Block) {
+    super(location);
+    this.condition = condition;
+    this.body = body;
+  }
+}
+
+export class For extends Statement {
+  variable: string;
+  container: Expression;
+  body: Block;
+  constructor(location: MLocation, variable: string, container: Expression, body: Block) {
+    super(location);
+    this.variable = variable;
+    this.container = container;
+    this.body = body;
+  }
+}
+
+export class If extends Statement {
+  pairs: [Expression, Block][];
+  fallback: Block | null;
+  constructor(
+      location: MLocation,
+      pairs: [Expression, Block][],
+      fallback: Block | null) {
+    super(location);
+    this.pairs = pairs;
+    this.fallback = fallback;
+  }
+}
+
+export class Block extends Statement {
+  statements: Statement[];
+  constructor(location: MLocation, statements: Statement[]) {
+    super(location);
+    this.statements = statements;
+  }
+}
+
+export class Return extends Statement {
+  expression: Expression;
+  constructor(location: MLocation, expression: Expression) {
+    super(location);
+    this.expression = expression;
+  }
+}
+
+export class ExpressionStatement extends Statement {
+  expression: Expression;
+  constructor(location: MLocation, expression: Expression) {
+    super(location);
+    this.expression = expression;
+  }
+}
+
+export class GetVariable extends Expression {
+  name: string;
+  constructor(location: MLocation, name: string) {
+    super(location);
+    this.name = name;
+  }
+}
+
+export class SetVariable extends Expression {
+  name: string;
+  value: Expression;
+  constructor(location: MLocation, name: string, value: Expression) {
+    super(location);
+    this.name = name;
+    this.value = value;
+  }
+}
+
+export abstract class Literal<T> extends Expression {
+  value: T;
+  constructor(location: MLocation, value: T) {
+    super(location);
+    this.value = value;
+  }
+}
+
+export class NilLiteral extends Literal<null> {}
+export class BoolLiteral extends Literal<boolean> {}
+export class NumberLiteral extends Literal<number> {}
+export class StringLiteral extends Literal<string> {}
+
+export class FunctionCall extends Expression {
+  func: Expression;
+  args: Expression[];
+  constructor(location: MLocation, func: Expression, args: Expression[]) {
+    super(location);
+    this.func = func;
+    this.args = args;
+  }
+}
+
+export class MethodCall extends Expression {
+  owner: Expression;
+  name: string;
+  args: Expression[];
+  constructor(location: MLocation, owner: Expression, name: string, args: Expression[]) {
+    super(location);
+    this.owner = owner;
+    this.name = name;
+    this.args = args;
+  }
+}
+
+export class GetField extends Expression {
+  owner: Expression;
+  name: string;
+  constructor(location: MLocation, owner: Expression, name: string) {
+    super(location);
+    this.owner = owner;
+    this.name = name;
+  }
+}
+
+export class SetField extends Expression {
+  owner: Expression;
+  name: string;
+  value: Expression;
+  constructor(location: MLocation, owner: Expression, name: string, value: Expression) {
+    super(location);
+    this.owner = owner;
+    this.name = name;
+    this.value = value;
+  }
+}
+
+export class Logical extends Expression {
+  op: LogicalOperator;
+  args: Expression[];
+  constructor(
+      location: MLocation,
+      op: LogicalOperator,
+      args: Expression[]) {
+    super(location);
+    this.op = op;
+    this.args = args;
   }
 }
--- misc/vscode/src/lang/parser.ts
+++ misc/vscode/src/lang/parser.ts
@@ -1,11 +1,54 @@
 import * as ast from './ast';
-import { MAst } from "./ast";
+import { Ast } from "./ast";
 import { MError } from "./error";
 import { MScanner } from "./scanner";
 import { MScope } from "./scope";
 import { MSymbolTable } from "./symbol";
 import { MToken, MTokenType } from "./token";
 
+const PrecList: MTokenType[][] = [
+  [],
+  ['or'],
+  ['and'],
+  [],        // precedence for unary operator 'not'
+  ['==', '!=', '<', '>', '<=', '>=', 'in', 'not', 'is'],
+  ['<<', '>>'],
+  ['&'],
+  ['^'],
+  ['|'],
+  ['+', '-'],
+  ['*', '/', '//', '%'],
+  [],        // precedence for unary operators '-', '+' and '~'
+  ['.', '(', '['],
+];
+const PrecMap: Map<MTokenType, number> = new Map();
+for (let i = 0; i < PrecList.length; i++) {
+  for (const tokenType of PrecList[i]) {
+    PrecMap.set(tokenType, i);
+  }
+}
+const PREC_UNARY_NOT = PrecMap.get('and')! + 1;
+const PREC_UNARY_MINUS = PrecMap.get('*')! + 1;
+const BinopMethodMap: Map<MTokenType, string> = new Map([
+  ['==', '__eq__'],
+  ['!=', '__ne__'],
+  ['<', '__lt__'],
+  ['<=', '__le__'],
+  ['>', '__gt__'],
+  ['>=', '__ge__'],
+  ['in', '__contains__'],
+  ['<<', '__lshift__'],
+  ['>>', '__rshift__'],
+  ['&', '__and__'],
+  ['^', '__xor__'],
+  ['|', '__or__'],
+  ['+', '__add__'],
+  ['-', '__sub__'],
+  ['*', '__mul__'],
+  ['/', '__div__'],
+  ['//', '__floordiv__'],
+  ['%', '__mod__'],
+])
 
 export class MParser {
   symbolTable: MSymbolTable;
@@ -22,19 +65,19 @@ export class MParser {
     this.peek = scanner.scanToken();
   }
 
-  newError(message: string) {
+  private newError(message: string) {
     throw new MError(this.peek.location, message);
   }
 
-  advance() {
+  private advance() {
     this.peek = this.scanner.scanToken();
   }
 
-  at(tokenType: MTokenType): boolean {
+  private at(tokenType: MTokenType): boolean {
     return this.peek.type === tokenType;
   }
 
-  consume(tokenType: MTokenType): boolean {
+  private consume(tokenType: MTokenType): boolean {
     if (this.at(tokenType)) {
       this.advance();
       return true;
@@ -42,46 +85,239 @@ export class MParser {
     return false;
   }
 
-  expect(tokenType: MTokenType, message: string | null = null) {
+  private expect(tokenType: MTokenType, message: string | null = null): MToken {
+    const token = this.peek;
     if (!this.consume(tokenType)) {
       if (!message) {
         message = `Expected '${tokenType}'`;
       }
       throw this.newError(`${message} but got '${this.peek.type}'`);
     }
+    return token;
   }
 
-  expectStatementDelimiter(message: string | null = null) {
+  private expectStatementDelimiter(message: string | null = null) {
     if (!this.consume('NEWLINE')) {
       this.expect(';', message);
     }
   }
 
-  parseForStatement(): MAst {
+  private parsePrimary(): Ast {
+    const location = this.peek.location;
+    if (this.consume('(')) {
+      const expression = this.parseExpression();
+      this.expect(')');
+      return expression;
+    } else if (this.at('NUMBER')) {
+      const expression = new ast.NumberLiteral(
+        location, <number>this.peek.value);
+      this.advance();
+      return expression;
+    } else if (this.at('STRING')) {
+      const expression = new ast.StringLiteral(
+        location, <string>this.peek.value);
+      this.advance();
+      return expression;
+    } else if (this.consume('true')) {
+      return new ast.BoolLiteral(location, true);
+    } else if (this.consume('false')) {
+      return new ast.BoolLiteral(location, false);
+    } else if (this.consume('nil')) {
+      return new ast.NilLiteral(location, null);
+    } else if (this.at('IDENTIFIER')) {
+      const name = <string>this.peek.value;
+      this.advance();
+      if (this.consume('=')) {
+        const value = this.parseExpression();
+        return new ast.SetVariable(location, name, value);
+      } else {
+        return new ast.GetVariable(location, name);
+      }
+    }
+    throw this.newError(
+      `Expected expression but got '${this.peek.type}'`);
+  }
+
+  private parseArguments(): Ast[] {
+    const args: Ast[] = [];
+    this.expect('(');
+    while (!this.at(')')) {
+      args.push(this.parseExpression());
+      if (!this.consume(',')) {
+        break;
+      }
+    }
+    this.expect(')');
+    return args;
+  }
+
+  private parsePrefix(): ast.Expression {
+    const location = this.peek.location;
+    switch (this.peek.type) {
+      case '(': {
+        this.advance();
+        const expression = this.parseExpression();
+        this.expect(')');
+        return expression;
+      }
+      case 'NUMBER': {
+        const expression = new ast.NumberLiteral(
+          location, <number>this.peek.value);
+        this.advance();
+        return expression;
+      }
+      case 'STRING': {
+        const expression = new ast.StringLiteral(
+          location, <string>this.peek.value);
+        this.advance();
+        return expression;
+      }
+      case 'true': {
+        this.advance();
+        return new ast.BoolLiteral(location, true);
+      }
+      case 'false': {
+        this.advance();
+        return new ast.BoolLiteral(location, false);
+      }
+      case 'nil': {
+        this.advance();
+        return new ast.NilLiteral(location, null);
+      }
+      case 'IDENTIFIER': {
+        const name = <string>this.peek.value;
+        this.advance();
+        if (this.consume('=')) {
+          const value = this.parseExpression();
+          return new ast.SetVariable(location, name, value);
+        } else {
+          return new ast.GetVariable(location, name);
+        }
+      }
+      case 'not': {
+        this.advance();
+        return new ast.Logical(location, 'not', [
+          this.parsePrec(PREC_UNARY_NOT)]);
+      }
+      case '~':
+      case '-':
+      case '+':
+        const tokenType = this.peek.type;
+        let methodName = 'invalid';
+        switch (tokenType) {
+          case '~':
+            methodName = '__not__';
+            break;
+          case '-':
+            methodName = '__neg__';
+            break;
+          case '+':
+            methodName = '__pos__';
+            break;
+          default:
+            throw this.newError(`assertionError ${tokenType}`);
+        }
+        this.advance();
+        return new ast.MethodCall(
+          location, this.parsePrec(PREC_UNARY_MINUS), methodName, []);
+    }
+    throw this.newError(
+      `Expected expression but got '${this.peek.type}'`);
+  }
+
+  parseInfix(lhs: ast.Expression): ast.Expression {
+    const location = this.peek.location;
+    const tokenType = this.peek.type;
+    const precedence = PrecMap.get(tokenType);
+    const methodName = BinopMethodMap.get(tokenType);
+    if (!precedence) {
+      throw this.newError('assertionError');
+    }
+
+    switch (tokenType) {
+      case '.': {
+        this.advance();
+        const token = this.expect(
+          'IDENTIFIER', 'Expected field or method identifier');
+        const nameLocation = token.location;
+        const name = <string>token.value;
+        if (this.at('(')) {
+          const args = this.parseArguments();
+          return new ast.MethodCall(nameLocation, lhs, name, args);
+        } else if (this.consume('=')) {
+          const value = this.parseExpression();
+          return new ast.SetField(nameLocation, lhs, name, value);
+        }
+        return new ast.GetField(nameLocation, lhs, name);
+      }
+      case '[': {
+        this.advance();
+        const index = this.parseExpression();
+        this.expect(']');
+        if (this.consume('=')) {
+          const value = this.parseExpression();
+          return new ast.MethodCall(
+            location, lhs, '__setitem__', [index, value]);
+        }
+        return new ast.MethodCall(location, lhs, '__getitem__', [index]);
+      }
+      case '(': {
+        this.advance();
+        const args = this.parseArguments();
+        return new ast.FunctionCall(location, lhs, args);
+      }
+    }
+
+    if (methodName) {
+      this.advance();
+      const rhs = this.parsePrec(precedence + 1);
+      return new ast.MethodCall(location, lhs, methodName, [rhs]);
+    }
+    if (tokenType === 'and' || tokenType === 'or') {
+      this.advance();
+      const rhs = this.parsePrec(precedence + 1);
+      return new ast.Logical(location, tokenType, [lhs, rhs]);
+    }
+    throw this.newError(`assertionError ${tokenType}`);
+  }
+
+  parsePrec(precedence: number): ast.Expression {
+    let expr = this.parsePrefix();
+    while (precedence <= (PrecMap.get(this.peek.type) || 0)) {
+      expr = this.parseInfix(expr);
+    }
+    return expr;
+  }
+
+  parseExpression(): Ast {
+    return this.parsePrec(1);
+  }
+
+  parseForStatement(): Ast {
     throw this.newError('TODO: parseForStatement');
   }
 
-  parseIfStatement(): MAst {
+  parseIfStatement(): Ast {
     throw this.newError('TODO: parseIfStatement');
   }
 
-  parseReturnStatement(): MAst {
+  parseReturnStatement(): Ast {
     throw this.newError('TODO: parseReturnStatement');
   }
 
-  parseWhileStatement(): MAst {
+  parseWhileStatement(): Ast {
     throw this.newError('TODO: parseWhileStatement');
   }
 
-  parseImportStatement(): MAst {
+  parseImportStatement(): Ast {
     throw this.newError('TODO: parseImportStatement');
   }
 
-  parseExpressionStatement(): MAst {
+  parseExpressionStatement(): Ast {
     throw this.newError('TODO: parseExpressionStatement');
   }
 
-  parseStatement(): MAst {
+  parseStatement(): Ast {
     if (this.at('for')) {
       return this.parseForStatement();
     } else if (this.at('if')) {
@@ -101,23 +337,23 @@ export class MParser {
         'Expected statement delimiter at end of pass statement');
       return node;
     } else {
-      return this.parseStatement();
+      return this.parseExpressionStatement();
     }
   }
 
-  parseClassDeclaration(): MAst {
+  parseClassDeclaration(): Ast {
     throw this.newError('TODO: parseClassDeclaration');
   }
 
-  parseFunctionDeclaration(): MAst {
+  parseFunctionDeclaration(): Ast {
     throw this.newError('TODO: parseFunctionDeclaration');
   }
 
-  parseVarDeclaration(): MAst {
+  parseVarDeclaration(): Ast {
     throw this.newError('TODO: parseVarDeclaration');
   }
 
-  parseDeclaration(): MAst {
+  parseDeclaration(): Ast {
     if (this.at('class')) {
       return this.parseClassDeclaration();
     } else if (this.at('def')) {
