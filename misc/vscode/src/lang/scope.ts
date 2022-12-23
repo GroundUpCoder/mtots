3dd7c04c4504c6c46ee96c95f9c3789a08fce8f8
Wombat Nomad <116476627+GroundUpCoder@users.noreply.github.com>
2022-12-23 23:08:20 +0700

    vscode: undo BaseScope

--- misc/vscode/src/lang/scope.ts
+++ misc/vscode/src/lang/scope.ts
@@ -1,12 +1,35 @@
-import { BaseScope } from "./solver/base/scope";
 import { MSymbol } from "./symbol";
 
 
 
-export class MScope extends BaseScope<MSymbol> {
+export class MScope {
+  parent: this | null;
+  map: Map<string, MSymbol>;
+
   static new(parent: MScope | null = null, map: Map<string, MSymbol> | null = null): MScope {
     const scope = new MScope(map);
     scope.parent = parent;
     return scope;
+  }
+
+  private constructor(map: Map<string, MSymbol> | null = null) {
+    this.parent = null;
+    this.map = map || new Map();
+  }
+
+  get(name: string): MSymbol | null {
+    let scope: this | null = this;
+    while (scope) {
+      const value = scope.map.get(name);
+      if (value) {
+        return value;
+      }
+      scope = scope.parent;
+    }
+    return null;
+  }
+
+  set(symbol: MSymbol) {
+    this.map.set(symbol.name, symbol);
   }
 }
--- misc/vscode/src/lang/solver/base/solver.ts
+++
@@ -1,5 +0,0 @@
-
-
-
-export class BaseSolver<Scope> {
-}
